ee9ebcc91aa1a582046401fb2835f5da3fefbcf8
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-14 15:47:42 +0200

    feat(meos): add EINTERSECTS/ETOUCHES/ECONTAINS/ACONTAINS/ATOUCHES_TGEO_GEO NES operators (W104)
    Wire five remaining tgeo predicate operators: EINTERSECTS_TGEO_GEO,
    ETOUCHES_TGEO_GEO, ECONTAINS_TGEO_GEO (ever-*) and ACONTAINS_TGEO_GEO,
    ATOUCHES_TGEO_GEO (always-*). All use the 4-arg pattern
    (lon:FLOAT64, lat:FLOAT64, ts:UINT64, wkt:VARCHAR) -> FLOAT64.

---
+++ nes-logical-operators/src/Functions/Meos/AtouchesTgeoGeoLogicalFunction.cpp
@@ -0,0 +1,107 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/AtouchesTgeoGeoLogicalFunction.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <ErrorHandling.hpp>
+#include <LogicalFunctionRegistry.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <fmt/format.h>
+#include <SerializableVariantDescriptor.pb.h>
+
+namespace NES
+{
+
+AtouchesTgeoGeoLogicalFunction::AtouchesTgeoGeoLogicalFunction(LogicalFunction lon, LogicalFunction lat,
+                                            LogicalFunction ts, LogicalFunction wkt)
+    : dataType(DataTypeProvider::provideDataType(DataType::Type::FLOAT64))
+{
+    parameters.reserve(4);
+    parameters.push_back(std::move(lon));
+    parameters.push_back(std::move(lat));
+    parameters.push_back(std::move(ts));
+    parameters.push_back(std::move(wkt));
+}
+
+DataType AtouchesTgeoGeoLogicalFunction::getDataType() const { return dataType; }
+
+LogicalFunction AtouchesTgeoGeoLogicalFunction::withDataType(const DataType& newDataType) const
+{
+    auto copy = *this; copy.dataType = newDataType; return copy;
+}
+
+std::vector<LogicalFunction> AtouchesTgeoGeoLogicalFunction::getChildren() const { return parameters; }
+
+LogicalFunction AtouchesTgeoGeoLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    PRECONDITION(children.size() == 4,
+                 "AtouchesTgeoGeoLogicalFunction requires 4 children, but got {}", children.size());
+    auto copy = *this; copy.parameters = children; return copy;
+}
+
+std::string_view AtouchesTgeoGeoLogicalFunction::getType() const { return NAME; }
+
+bool AtouchesTgeoGeoLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (const auto* other = dynamic_cast<const AtouchesTgeoGeoLogicalFunction*>(&rhs))
+        return parameters == other->parameters;
+    return false;
+}
+
+std::string AtouchesTgeoGeoLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return fmt::format("{}({})", NAME, parameters[0].explain(verbosity));
+}
+
+LogicalFunction AtouchesTgeoGeoLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    std::vector<LogicalFunction> c;
+    c.reserve(4);
+    c.emplace_back(parameters[0].withInferredDataType(schema));
+    c.emplace_back(parameters[1].withInferredDataType(schema));
+    c.emplace_back(parameters[2].withInferredDataType(schema));
+    c.emplace_back(parameters[3].withInferredDataType(schema));
+    INVARIANT(c[0].getDataType().isType(DataType::Type::FLOAT64), "lon must be FLOAT64");
+    INVARIANT(c[1].getDataType().isType(DataType::Type::FLOAT64), "lat must be FLOAT64");
+    INVARIANT(c[2].getDataType().isType(DataType::Type::UINT64),  "ts must be UINT64");
+    INVARIANT(c[3].getDataType().isType(DataType::Type::VARSIZED),"wkt must be VARSIZED");
+    return withChildren(c);
+}
+
+SerializableFunction AtouchesTgeoGeoLogicalFunction::serialize() const
+{
+    SerializableFunction proto;
+    proto.set_function_type(std::string(NAME));
+    DataTypeSerializationUtil::serializeDataType(dataType, proto.mutable_data_type());
+    for (const auto& child : parameters)
+        proto.add_children()->CopyFrom(child.serialize());
+    return proto;
+}
+
+LogicalFunctionRegistryReturnType LogicalFunctionGeneratedRegistrar::RegisterAtouchesTgeoGeoLogicalFunction(
+    LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.children.size() == 4,
+                 "AtouchesTgeoGeoLogicalFunction requires 4 children but got {}",
+                 arguments.children.size());
+    return AtouchesTgeoGeoLogicalFunction(std::move(arguments.children[0]),
+                                 std::move(arguments.children[1]),
+                                 std::move(arguments.children[2]),
+                                 std::move(arguments.children[3]));
+}
+
+} // namespace NES
