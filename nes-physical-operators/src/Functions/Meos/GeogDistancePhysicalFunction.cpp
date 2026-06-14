5cad8c49f9093a27d0fb8870256a5c1f65f24b50
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-15 00:13:39 +0200

    feat(meos): add GEO_NUM_POINTS/GEOS/SRID/IS_UNITARY, GEO_EQUALS/SAME, GEOG_DISTANCE NES operators (W105)
    Wire seven static geometry scalar operators: GEO_NUM_POINTS, GEO_NUM_GEOS,
    GEO_SRID, GEO_IS_UNITARY (1-arg VARCHAR->FLOAT64) and GEO_EQUALS, GEO_SAME,
    GEOG_DISTANCE (2-arg VARCHAR*VARCHAR->FLOAT64).

---
+++ nes-physical-operators/src/Functions/Meos/GeogDistancePhysicalFunction.cpp
@@ -0,0 +1,78 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/GeogDistancePhysicalFunction.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <MEOSWrapper.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/DataTypes/VariableSizedData.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <function.hpp>
+#include <string>
+#include <utility>
+#include <val.hpp>
+
+extern "C" {
+#include <meos.h>
+#include <meos_geo.h>
+}
+
+namespace NES {
+
+GeogDistancePhysicalFunction::GeogDistancePhysicalFunction(PhysicalFunction wkt1, PhysicalFunction wkt2)
+{
+    paramFns.reserve(2);
+    paramFns.push_back(std::move(wkt1));
+    paramFns.push_back(std::move(wkt2));
+}
+
+VarVal GeogDistancePhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    auto s1 = paramFns[0].execute(record, arena).cast<VariableSizedData>();
+    auto s2 = paramFns[1].execute(record, arena).cast<VariableSizedData>();
+
+    const auto result = nautilus::invoke(
+        +[](const char* w1, uint32_t w1sz, const char* w2, uint32_t w2sz) -> double {
+            try {
+                MEOS::Meos::ensureMeosInitialized();
+                std::string s1(w1, w1sz);
+                std::string s2(w2, w2sz);
+                GSERIALIZED* gs1 = geom_in(s1.c_str(), -1);
+                if (!gs1) return 0.0;
+                GSERIALIZED* gs2 = geom_in(s2.c_str(), -1);
+                if (!gs2) { free(gs1); return 0.0; }
+                double r = geog_distance(gs1, gs2);
+                free(gs1); free(gs2);
+                return r;
+            } catch (const std::exception&) { return 0.0; }
+        },
+        s1, s2);
+
+    return VarVal(result);
+}
+
+PhysicalFunctionRegistryReturnType PhysicalFunctionGeneratedRegistrar::RegisterGeogDistancePhysicalFunction(
+    PhysicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.childFunctions.size() == 2,
+                 "GeogDistancePhysicalFunction requires 2 children but got {}",
+                 arguments.childFunctions.size());
+    return GeogDistancePhysicalFunction(std::move(arguments.childFunctions[0]),
+                                  std::move(arguments.childFunctions[1]));
+}
+
+} // namespace NES
