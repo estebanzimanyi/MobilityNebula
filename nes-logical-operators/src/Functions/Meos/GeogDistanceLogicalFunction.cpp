dd0200f7a8e2b7c920a725308cd146413dbebb53
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-15 00:13:39 +0200

    feat(meos): add GEO_NUM_POINTS/GEOS/SRID/IS_UNITARY, GEO_EQUALS/SAME, GEOG_DISTANCE NES operators (W105)
    Wire seven static geometry scalar operators: GEO_NUM_POINTS, GEO_NUM_GEOS,
    GEO_SRID, GEO_IS_UNITARY (1-arg VARCHAR->FLOAT64) and GEO_EQUALS, GEO_SAME,
    GEOG_DISTANCE (2-arg VARCHAR*VARCHAR->FLOAT64).

---
+++ nes-logical-operators/src/Functions/Meos/GeogDistanceLogicalFunction.cpp
@@ -0,0 +1,98 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/GeogDistanceLogicalFunction.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <ErrorHandling.hpp>
+#include <LogicalFunctionRegistry.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <fmt/format.h>
+#include <SerializableVariantDescriptor.pb.h>
+
+namespace NES
+{
+
+GeogDistanceLogicalFunction::GeogDistanceLogicalFunction(LogicalFunction wkt1, LogicalFunction wkt2)
+    : dataType(DataTypeProvider::provideDataType(DataType::Type::FLOAT64))
+{
+    parameters.reserve(2);
+    parameters.push_back(std::move(wkt1));
+    parameters.push_back(std::move(wkt2));
+}
+
+DataType GeogDistanceLogicalFunction::getDataType() const { return dataType; }
+
+LogicalFunction GeogDistanceLogicalFunction::withDataType(const DataType& newDataType) const
+{
+    auto copy = *this; copy.dataType = newDataType; return copy;
+}
+
+std::vector<LogicalFunction> GeogDistanceLogicalFunction::getChildren() const { return parameters; }
+
+LogicalFunction GeogDistanceLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    PRECONDITION(children.size() == 2,
+                 "GeogDistanceLogicalFunction requires 2 children, but got {}", children.size());
+    auto copy = *this; copy.parameters = children; return copy;
+}
+
+std::string_view GeogDistanceLogicalFunction::getType() const { return NAME; }
+
+bool GeogDistanceLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (const auto* other = dynamic_cast<const GeogDistanceLogicalFunction*>(&rhs))
+        return parameters == other->parameters;
+    return false;
+}
+
+std::string GeogDistanceLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return fmt::format("{}({})", NAME, parameters[0].explain(verbosity));
+}
+
+LogicalFunction GeogDistanceLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    std::vector<LogicalFunction> c;
+    c.reserve(2);
+    c.emplace_back(parameters[0].withInferredDataType(schema));
+    c.emplace_back(parameters[1].withInferredDataType(schema));
+    INVARIANT(c[0].getDataType().isType(DataType::Type::VARSIZED), "wkt1 must be VARSIZED");
+    INVARIANT(c[1].getDataType().isType(DataType::Type::VARSIZED), "wkt2 must be VARSIZED");
+    return withChildren(c);
+}
+
+SerializableFunction GeogDistanceLogicalFunction::serialize() const
+{
+    SerializableFunction proto;
+    proto.set_function_type(std::string(NAME));
+    DataTypeSerializationUtil::serializeDataType(dataType, proto.mutable_data_type());
+    for (const auto& child : parameters)
+        proto.add_children()->CopyFrom(child.serialize());
+    return proto;
+}
+
+LogicalFunctionRegistryReturnType LogicalFunctionGeneratedRegistrar::RegisterGeogDistanceLogicalFunction(
+    LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.children.size() == 2,
+                 "GeogDistanceLogicalFunction requires 2 children but got {}",
+                 arguments.children.size());
+    return GeogDistanceLogicalFunction(std::move(arguments.children[0]),
+                                 std::move(arguments.children[1]));
+}
+
+} // namespace NES
