8c260dc73341258598b991c0792a956a2cf08a2e
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-15 06:36:12 +0200

    feat(meos): add tcbuffer_tcbuffer per-event scalar operators (W116)
    Wires twelve operators over two tcbuffer instants using a new 8-arg all-numeric
    pattern (lon1:FLOAT64, lat1:FLOAT64, r1:FLOAT64, ts1:UINT64, lon2:FLOAT64,
    lat2:FLOAT64, r2:FLOAT64, ts2:UINT64) -> FLOAT64. The physical functions build
    two tcbuffer instants independently then delegate to the MEOS kernel. Operators:
    EVER_EQ/ALWAYS_EQ, EVER_NE/ALWAYS_NE, EINTERSECTS/AINTERSECTS,
    ECOVERS/ACOVERS, ADISJOINT, ETOUCHES/ATOUCHES _TCBUFFER_TCBUFFER,
    and NAD_TCBUFFER_TCBUFFER.

---
+++ nes-logical-operators/src/Functions/Meos/EverEqTcbufferTcbufferLogicalFunction.cpp
@@ -0,0 +1,118 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/EverEqTcbufferTcbufferLogicalFunction.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <ErrorHandling.hpp>
+#include <LogicalFunctionRegistry.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <fmt/format.h>
+#include <SerializableVariantDescriptor.pb.h>
+
+namespace NES
+{
+
+EverEqTcbufferTcbufferLogicalFunction::EverEqTcbufferTcbufferLogicalFunction(
+    LogicalFunction lon1, LogicalFunction lat1, LogicalFunction r1, LogicalFunction ts1,
+    LogicalFunction lon2, LogicalFunction lat2, LogicalFunction r2, LogicalFunction ts2)
+    : dataType(DataTypeProvider::provideDataType(DataType::Type::FLOAT64))
+{
+    parameters.reserve(8);
+    parameters.push_back(std::move(lon1));
+    parameters.push_back(std::move(lat1));
+    parameters.push_back(std::move(r1));
+    parameters.push_back(std::move(ts1));
+    parameters.push_back(std::move(lon2));
+    parameters.push_back(std::move(lat2));
+    parameters.push_back(std::move(r2));
+    parameters.push_back(std::move(ts2));
+}
+
+DataType EverEqTcbufferTcbufferLogicalFunction::getDataType() const { return dataType; }
+
+LogicalFunction EverEqTcbufferTcbufferLogicalFunction::withDataType(const DataType& newDataType) const
+{
+    auto copy = *this; copy.dataType = newDataType; return copy;
+}
+
+std::vector<LogicalFunction> EverEqTcbufferTcbufferLogicalFunction::getChildren() const { return parameters; }
+
+LogicalFunction EverEqTcbufferTcbufferLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    PRECONDITION(children.size() == 8,
+                 "EverEqTcbufferTcbufferLogicalFunction requires 8 children, but got {}", children.size());
+    auto copy = *this; copy.parameters = children; return copy;
+}
+
+std::string_view EverEqTcbufferTcbufferLogicalFunction::getType() const { return NAME; }
+
+bool EverEqTcbufferTcbufferLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (const auto* other = dynamic_cast<const EverEqTcbufferTcbufferLogicalFunction*>(&rhs))
+        return parameters == other->parameters;
+    return false;
+}
+
+std::string EverEqTcbufferTcbufferLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return fmt::format("{}({})", NAME, parameters[0].explain(verbosity));
+}
+
+LogicalFunction EverEqTcbufferTcbufferLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    std::vector<LogicalFunction> c;
+    c.reserve(8);
+    for (const auto& p : parameters)
+        c.emplace_back(p.withInferredDataType(schema));
+    INVARIANT(c[0].getDataType().isType(DataType::Type::FLOAT64), "lon1 must be FLOAT64");
+    INVARIANT(c[1].getDataType().isType(DataType::Type::FLOAT64), "lat1 must be FLOAT64");
+    INVARIANT(c[2].getDataType().isType(DataType::Type::FLOAT64), "r1 must be FLOAT64");
+    INVARIANT(c[3].getDataType().isType(DataType::Type::UINT64),  "ts1 must be UINT64");
+    INVARIANT(c[4].getDataType().isType(DataType::Type::FLOAT64), "lon2 must be FLOAT64");
+    INVARIANT(c[5].getDataType().isType(DataType::Type::FLOAT64), "lat2 must be FLOAT64");
+    INVARIANT(c[6].getDataType().isType(DataType::Type::FLOAT64), "r2 must be FLOAT64");
+    INVARIANT(c[7].getDataType().isType(DataType::Type::UINT64),  "ts2 must be UINT64");
+    return withChildren(c);
+}
+
+SerializableFunction EverEqTcbufferTcbufferLogicalFunction::serialize() const
+{
+    SerializableFunction proto;
+    proto.set_function_type(std::string(NAME));
+    DataTypeSerializationUtil::serializeDataType(dataType, proto.mutable_data_type());
+    for (const auto& child : parameters)
+        proto.add_children()->CopyFrom(child.serialize());
+    return proto;
+}
+
+LogicalFunctionRegistryReturnType LogicalFunctionGeneratedRegistrar::RegisterEverEqTcbufferTcbufferLogicalFunction(
+    LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.children.size() == 8,
+                 "EverEqTcbufferTcbufferLogicalFunction requires 8 children but got {}",
+                 arguments.children.size());
+    return EverEqTcbufferTcbufferLogicalFunction(std::move(arguments.children[0]),
+                                 std::move(arguments.children[1]),
+                                 std::move(arguments.children[2]),
+                                 std::move(arguments.children[3]),
+                                 std::move(arguments.children[4]),
+                                 std::move(arguments.children[5]),
+                                 std::move(arguments.children[6]),
+                                 std::move(arguments.children[7]));
+}
+
+} // namespace NES
