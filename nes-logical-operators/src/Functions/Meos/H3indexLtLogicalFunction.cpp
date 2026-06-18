6606a4555d4d91df400807909b4c4bf28603e789
Esteban Zimanyi <estebanzimanyi@gmail.com>
2026-06-18 13:02:36 +0200

    feat(meos): add H3Index static comparator NES operators (W138)
    Wires H3INDEX_EQ, H3INDEX_NE, H3INDEX_LT, H3INDEX_LE, H3INDEX_GT,
    H3INDEX_GE, and H3INDEX_CMP as 2-arg (a:UINT64, b:UINT64) -> FLOAT64
    operators backed by h3index_eq/ne/lt/le/gt/ge (bool -> 0.0/1.0) and
    h3index_cmp (int -> -1.0/0.0/1.0). New in pin ecosystem-pin-2026-06-18b.

---
+++ nes-logical-operators/src/Functions/Meos/H3indexLtLogicalFunction.cpp
@@ -0,0 +1,99 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/H3indexLtLogicalFunction.hpp>
+
+#include <DataTypes/DataType.hpp>
+#include <DataTypes/DataTypeProvider.hpp>
+#include <DataTypes/Schema.hpp>
+#include <ErrorHandling.hpp>
+#include <LogicalFunctionRegistry.hpp>
+#include <Serialization/DataTypeSerializationUtil.hpp>
+#include <fmt/format.h>
+#include <SerializableVariantDescriptor.pb.h>
+
+namespace NES
+{
+
+H3indexLtLogicalFunction::H3indexLtLogicalFunction(LogicalFunction a, LogicalFunction b)
+    : dataType(DataTypeProvider::provideDataType(DataType::Type::FLOAT64))
+{
+    parameters.reserve(2);
+    parameters.push_back(std::move(a));
+    parameters.push_back(std::move(b));
+}
+
+DataType H3indexLtLogicalFunction::getDataType() const { return dataType; }
+
+LogicalFunction H3indexLtLogicalFunction::withDataType(const DataType& newDataType) const
+{
+    auto copy = *this; copy.dataType = newDataType; return copy;
+}
+
+std::vector<LogicalFunction> H3indexLtLogicalFunction::getChildren() const { return parameters; }
+
+LogicalFunction H3indexLtLogicalFunction::withChildren(const std::vector<LogicalFunction>& children) const
+{
+    PRECONDITION(children.size() == 2,
+                 "H3indexLtLogicalFunction requires 2 children, but got {}", children.size());
+    auto copy = *this; copy.parameters = children; return copy;
+}
+
+std::string_view H3indexLtLogicalFunction::getType() const { return NAME; }
+
+bool H3indexLtLogicalFunction::operator==(const LogicalFunctionConcept& rhs) const
+{
+    if (const auto* other = dynamic_cast<const H3indexLtLogicalFunction*>(&rhs))
+        return parameters == other->parameters;
+    return false;
+}
+
+std::string H3indexLtLogicalFunction::explain(ExplainVerbosity verbosity) const
+{
+    return fmt::format("{}({})", NAME, parameters[0].explain(verbosity));
+}
+
+LogicalFunction H3indexLtLogicalFunction::withInferredDataType(const Schema& schema) const
+{
+    std::vector<LogicalFunction> c;
+    c.reserve(2);
+    for (const auto& p : parameters)
+        c.emplace_back(p.withInferredDataType(schema));
+    INVARIANT(c[0].getDataType().isType(DataType::Type::UINT64), "a must be UINT64");
+    INVARIANT(c[1].getDataType().isType(DataType::Type::UINT64), "b must be UINT64");
+    return withChildren(c);
+}
+
+SerializableFunction H3indexLtLogicalFunction::serialize() const
+{
+    SerializableFunction proto;
+    proto.set_function_type(std::string(NAME));
+    DataTypeSerializationUtil::serializeDataType(dataType, proto.mutable_data_type());
+    for (const auto& child : parameters)
+        proto.add_children()->CopyFrom(child.serialize());
+    return proto;
+}
+
+LogicalFunctionRegistryReturnType LogicalFunctionGeneratedRegistrar::RegisterH3indexLtLogicalFunction(
+    LogicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.children.size() == 2,
+                 "H3indexLtLogicalFunction requires 2 children but got {}",
+                 arguments.children.size());
+    return H3indexLtLogicalFunction(
+                                 std::move(arguments.children[0]),
+                                 std::move(arguments.children[1]));
+}
+
+} // namespace NES
---
+++ nes-physical-operators/src/Functions/Meos/H3indexEqPhysicalFunction.cpp
@@ -0,0 +1,68 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <Functions/Meos/H3indexEqPhysicalFunction.hpp>
+#include <Functions/PhysicalFunction.hpp>
+#include <MEOSWrapper.hpp>
+#include <Nautilus/DataTypes/VarVal.hpp>
+#include <Nautilus/Interface/Record.hpp>
+#include <PhysicalFunctionRegistry.hpp>
+#include <ErrorHandling.hpp>
+#include <ExecutionContext.hpp>
+#include <function.hpp>
+#include <utility>
+#include <val.hpp>
+
+extern "C" {
+#include <meos.h>
+#include <meos_h3.h>
+}
+
+namespace NES {
+
+H3indexEqPhysicalFunction::H3indexEqPhysicalFunction(PhysicalFunction a, PhysicalFunction b)
+{
+    paramFns.reserve(2);
+    paramFns.push_back(std::move(a));
+    paramFns.push_back(std::move(b));
+}
+
+VarVal H3indexEqPhysicalFunction::execute(const Record& record, ArenaRef& arena) const
+{
+    auto a = paramFns[0].execute(record, arena).cast<uint64_t>();
+    auto b = paramFns[1].execute(record, arena).cast<uint64_t>();
+    const auto result = nautilus::invoke(
+        +[](uint64_t a, uint64_t b) -> double {
+            try {
+                MEOS::Meos::ensureMeosInitialized();
+                bool r = h3index_eq((H3Index)a, (H3Index)b);
+                return r ? 1.0 : 0.0;
+            } catch (const std::exception&) { return 0.0; }
+        },
+        a, b);
+    return VarVal(result);
+}
+
+PhysicalFunctionRegistryReturnType PhysicalFunctionGeneratedRegistrar::RegisterH3indexEqPhysicalFunction(
+    PhysicalFunctionRegistryArguments arguments)
+{
+    PRECONDITION(arguments.childFunctions.size() == 2,
+                 "H3indexEqPhysicalFunction requires 2 children but got {}",
+                 arguments.childFunctions.size());
+    return H3indexEqPhysicalFunction(
+                                  std::move(arguments.childFunctions[0]),
+                                  std::move(arguments.childFunctions[1]));
+}
+
+} // namespace NES
